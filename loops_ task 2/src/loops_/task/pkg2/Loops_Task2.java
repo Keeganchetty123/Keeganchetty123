f16859a02df2a63e971a73e4a2f3073db65b9fb1
Keeganchetty123 <105056340+Keeganchetty123@users.noreply.github.com>
2022-06-08 10:32:15 +0200

    Add files via upload

---
+++ loops_ task 2/src/loops_/task/pkg2/Loops_Task2.java
@@ -0,0 +1,70 @@
+
+package loops_.task.pkg2;
+
+import javax.swing.JOptionPane;
+
+
+public class Loops_Task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+         JOptionPane.showMessageDialog(null, "Welcom to  EasyKanban!");
+       
+         
+         String String = JOptionPane.showInputDialog( null, " Enter Task name");
+         
+        
+        JOptionPane.showInputDialog( null, "Task number");
+         
+         JOptionPane.showMessageDialog(null, " For the next step,make sure your Description is less than 50 characters");
+        
+        JOptionPane.showInputDialog( null," Task Description ");
+        
+        
+        JOptionPane.showInputDialog(null, "Developer Details ");
+        
+        
+        JOptionPane.showInputDialog(null, " Task Duration ");
+         
+        
+        JOptionPane.showInputDialog(null, "Task ID");
+       
+        
+        JOptionPane.showInputDialog(null, " Task Status");
+        
+         JOptionPane.showMessageDialog(null, "Task successfully captured ");
+         
+         
+         
+         
+         
+         
+     
+              
+        
+         
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    
+    
+}
